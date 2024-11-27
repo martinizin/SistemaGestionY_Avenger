fb9815344d42c94fa56b62ad8f0accf38d213718
Eduardo Andrade <ejac09102006@gmail.com>
2024-11-27 16:38:53 -0500

    GUIform modificado

--- src/examenGUI.java
+++ src/examenGUI.java
@@ -1,7 +1,19 @@
 import javax.swing.*;
 
 public class examenGUI {
-    private JButton button1;
-    private JTextArea textArea1;
-    private JTextField textField1;
+    private JButton agregarBtn;
+    private JTextArea txtResultado;
+    private JTextField txtCodigo;
+    private JPanel pGeneral;
+    private JTextField txtNombre;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JButton buscarBtn;
+    private JTable table1;
+    private JButton ordenarBtn;
+    private JButton avengersConPoderButton;
+    private JButton avengersSinPoderButton;
+    private JButton mostrarAvengersButton;
+    private JButton calcularMisionesButton;
 }
