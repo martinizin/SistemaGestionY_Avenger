566b8c1172f8468e16abbcd7d6b8dec752ca2702
Eduardo Andrade <ejac09102006@gmail.com>
2024-11-27 16:46:18 -0500

    Listener creados

--- src/examenGUI.java
+++ src/examenGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class examenGUI {
     private JButton agregarBtn;
@@ -16,4 +18,49 @@ public class examenGUI {
     private JButton avengersSinPoderButton;
     private JButton mostrarAvengersButton;
     private JButton calcularMisionesButton;
+
+    public examenGUI() {
+        agregarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        mostrarAvengersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buscarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        avengersSinPoderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        avengersConPoderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ordenarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        calcularMisionesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
